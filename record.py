76f0400d53f8c82772414298bbc22be8ae8ac30e
Yi Chen <happyyi008@gmail.com>
2018-01-01 17:53:59 -0500

    refactor and add show option

--- record.py
+++ record.py
@@ -11,77 +11,106 @@ import os
 #
 
 catagories = [
-    "sleep",
-    "study",
+    "code",
     "cook",
-    "shop",
-    "exercise",
-    "eating",
     "chill",
-    "watching-smt",
-    "transit",
+    "eat",
     "exam",
+    "exercise",
+    "health",
+    "prep",
+    "shop",
+    "sleep",
     "social",
-    "coding",
+    "study",
+    "transit",
+    "watching-smt",
 ]
+
+record_file = os.path.expanduser('~/.record')
 
 def main():
     args = sys.argv
-    record_file = os.path.expanduser('~/.record')
-    fieldnames = ['date'] + [str(x) for x in range(0,24)]
 
-    if len(args) < 3:
+    date_today = get_date(args)
+    if args[1] == 'show':
+        print_day(str(date_today))
+        return
+
+    if len(args) < 3 and args[1] != 'show':
         print "You're missing something"
         return
 
     new_activity = args[1]
 
     if new_activity not in catagories:
-        print "activity {} is not in list".format(args[2])
+        print 'activity {} is not in list'.format(args[2])
         return
 
-    start, end = (int(i) for i in args[2].split('-'))
+    record_activity(new_activity, args[2], date_today)
+
+
+def record_activity(new_activity, interval, date):
+    fieldnames = ['date'] + [str(x) for x in range(0,24)]
+    start, end = (int(i) for i in interval.split('-'))
     if start >= end:
-        print "invalid time interval {}-{}".format(start, end)
+        print 'invalid time interval {}-{}'.format(start, end)
 
-    date_today = datetime.date.today()
-    if len(args) == 4:
-        if args[3] == 'yesterday':
-            date_today = datetime.date.today() - datetime.timedelta(days=1)
-        else:
-            date_today = datetime.datetime.strptime(args[3],"%Y-%m-%d").date()
-
-    activity = {'date':str(date_today)}
-
-    activity_list = list()
-    file_exists = os.path.exists(record_file)
-    if file_exists:
-        with open(record_file, 'rb') as csvfile:
-            reader = csv.DictReader(csvfile)
-            activity_list.extend(reader)
-        
+    record_list = load_record()
     with open(record_file, 'wb+') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         
         modifying = False
-        for line, row in enumerate(activity_list):
+        for line, row in enumerate(record_list):
             modified_row = row
-            if row['date'] == str(date_today):
-                modified_row = modify_activity(row, start, end, new_activity)
+            if row['date'] == str(date):
+                modified_row = modify_record(row, start, end, new_activity)
                 modifying = True
             writer.writerow(modified_row)
    
         if not modifying:
-            activity = modify_activity(activity, start, end, new_activity)
+            activity = {'date':str(date)}
+            activity = modify_record(activity, start, end, new_activity)
             writer.writerow(activity)
 
 
-def modify_activity(activity, start, end, activity_name):
+def get_date(args):
+    date_today = datetime.date.today()
+    if len(args) == 4:
+        if args[3] == 'yesterday':
+            date_today = datetime.date.today() - datetime.timedelta(days=1)
+        else:
+            date_today = datetime.datetime.strptime(args[3],'%Y-%m-%d').date()
+    return date_today
+
+def load_record():
+    record_list = list()
+    if not os.path.exists(record_file):
+        return record_list
+    with open(record_file, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        record_list.extend(reader)
+    return record_list
+
+def modify_record(activity, start, end, activity_name):
     for i in range(start, end):
         activity[str(i)] = activity_name
     return activity
 
-if __name__ == "__main__":
+def print_day(day):
+    record_list = load_record()
+    record = get_record(record_list, day)
+    print "Today: {}".format(record['date'])
+    for t in range(0, 24):
+        print '{}: {}'.format(str(t).rjust(3), record.get(str(t)) or 'nothing')
+
+def get_record(record_list, day):
+    for line, row in enumerate(record_list):
+        if row['date'] == day:
+            return record_list[line]
+    return dict()
+
+if __name__ == '__main__':
     main()
 
